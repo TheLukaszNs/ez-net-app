66f64336c6af840b87fc218804b3916bd02af20c
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2018-02-21 21:34:10 +0100

    Finished code base update

---
+++ assets/styles/loaderStyle.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const TYPE = 'Wave';
+const COLOR = '#3F51B5';
+const SIZE = 100;
+
+const style = StyleSheet.create({
+  loader: { marginTop: '50%' },
+});
+
+export { style, TYPE, COLOR, SIZE };
